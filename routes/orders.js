810475ded2b0ffaf9b6683a11683d35d9dec6c84
Karyna Kutsyia <Karyna_Kutsyia@epam.com>
2020-01-27 15:32:57 +0300

    implemented route protection

--- routes/orders.js
+++ routes/orders.js
@@ -2,7 +2,9 @@ const { Router } = require('express');
 const Order = require('../models/order');
 const router = Router();
 
-router.get('/', async (req, res) => {
+const auth = require('../middleware/auth');
+
+router.get('/', auth, async (req, res) => {
     try {
         const orders = await Order.find({ 'user.userId': req.user._id })
             .populate('user.userId');
@@ -20,7 +22,7 @@ router.get('/', async (req, res) => {
     }
 })
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
     try {
         const user = await req.user
             .populate('cart.items.courseId')
