21e691ad59a967137855f163d081a60748801377
Karyna Kutsyia <Karyna_Kutsyia@epam.com>
2020-01-27 16:16:43 +0300

    implemented register user

--- routes/auth.js
+++ routes/auth.js
@@ -19,12 +19,35 @@ router.get('/logout', (req, res) => {
 
 router.post('/login', async (req, res) => {
     const user = await User.findById('5e2aa7f0c2981a2d60929b21');
+
     req.session.user = user;
     req.session.isAuthenticated = true;
     req.session.save(err => {
         if (err) throw err;
         res.redirect('/');
     })
+});
+
+router.post('/register', async (req, res) => {
+    try {
+        const { email, password, confirm, name } = req.body;
+        const candidate = await User.findOne({ email });
+
+        if (candidate) {
+            res.redirect('/auth/login#register');
+        } else {
+            const user = new User({
+                email,
+                name,
+                password,
+                cart: { items: []}
+            });
+            await user.save();
+            res.redirect('/auth/login#login');
+        };
+    } catch(err) {
+        console.log(err);
+    };
 });
 
 module.exports = router;
