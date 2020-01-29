b7dc40e483da3299ad7ad077ee6ea560578f5361
Karyna Kutsyia <Karyna_Kutsyia@epam.com>
2020-01-29 14:51:31 +0300

    improve data for register

--- utils/validators.js
+++ utils/validators.js
@@ -2,20 +2,28 @@ const { check } = require('express-validator');
 const User = require('../models/user');
 
 exports.registerValidators = [
-    check('email').isEmail().withMessage('Enter correct email.').custom(async (value, { req }) => {
+    check('email')
+        .isEmail().withMessage('Enter correct email.')
+        .custom(async (value, { req }) => {
         try {
             const user = await User.findOne({ email: value });
             if (user)  {
                 return Promise.reject('Such email exists');
             }
-        } catch(err) {
-            console.log(err);
-        }
-    }),
-    check('password', 'Enter correct password. It should include min 6 simbol.').isLength({ min:6, max: 56 }).isAlphanumeric(),
-    check('confirm').custom((value, { req }) => {
+        } catch(err) { console.log(err) }
+        })
+        .normalizeEmail(),
+    check('password', 'Enter correct password. It should include min 6 simbol.')
+        .isLength({ min:6, max: 56 })
+        .isAlphanumeric()
+        .trim(),
+    check('confirm')
+        .custom((value, { req }) => {
         if(value !== req.body.password) throw new Error ('Passwords must match.');
         return true;
-    }),
-    check('name').isLength({ min:3 }).withMessage('Name should include min 3 simbol.')
+        })
+        .trim(),
+    check('name')
+        .isLength({ min:3 }).withMessage('Name should include min 3 simbol.')
+        .trim()
 ];
