e47fbb870bb4b4a9fdbfdc81f58d78eea01ff88d
Karyna Kutsyia <Karyna_Kutsyia@epam.com>
2020-01-29 14:14:07 +0300

    implemented async validation

--- routes/auth.js
+++ routes/auth.js
@@ -53,8 +53,7 @@ router.post('/login', async (req, res) => {
 
 router.post('/register', registerValidators, async (req, res) => {
     try {
-        const { email, password, confirm, name } = req.body;
-        const candidate = await User.findOne({ email });
+        const { email, password, name } = req.body;
         const errors = validationResult(req);
         
         if (!errors.isEmpty()) {
@@ -62,20 +61,15 @@ router.post('/register', registerValidators, async (req, res) => {
             return res.status(422).redirect('/auth/login#register');//ошибка валидации 422
         }
 
-        if (candidate) {
-            req.flash('registerError', 'Such email exists');
-            res.redirect('/auth/login#register');
-        } else {
-            const hashPassword = await bcrypt.hash(password, 10); //число для более сложного шифрования, чем больше строка тем сложнее взломать
-            const user = new User({
-                email,
-                name,
-                password: hashPassword,
-                cart: { items: [] }
-            });
-            await user.save();
-            res.redirect('/auth/login#login');
-        };
+        const hashPassword = await bcrypt.hash(password, 10); //число для более сложного шифрования, чем больше строка тем сложнее взломать
+        const user = new User({
+            email,
+            name,
+            password: hashPassword,
+            cart: { items: [] }
+        });
+        await user.save();
+        res.redirect('/auth/login#login');
     } catch(err) {
         console.log(err);
     };
--- utils/validators.js
+++ utils/validators.js
@@ -1,7 +1,17 @@
 const { check } = require('express-validator');
+const User = require('../models/user');
 
 exports.registerValidators = [
-    check('email').isEmail().withMessage('Enter correct email.'),
+    check('email').isEmail().withMessage('Enter correct email.').custom(async (value, { req }) => {
+        try {
+            const user = await User.findOne({ email: value });
+            if (user)  {
+                return Promise.reject('Such email exists');
+            }
+        } catch(err) {
+            console.log(err);
+        }
+    }),
     check('password', 'Enter correct password. It should include min 6 simbol.').isLength({ min:6, max: 56 }).isAlphanumeric(),
     check('confirm').custom((value, { req }) => {
         if(value !== req.body.password) throw new Error ('Passwords must match.');
